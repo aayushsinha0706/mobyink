0cee18371e6581f4dd8bab95c90b0e8f54b286a4
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2024-12-10 16:49:30 +0530

    transforming font

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,51 @@ const App = () => {
 
   const [selectFont, setSelectFont] = useState(fonts[0]);
   const [text,setText] = useState('')
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setIsSubmitted(true)
+  }
+
+  if (isSubmitted) {
+    return (
+      <div>
+        <p style = {{fontFamily: selectFont, fontSize: '24px'}}>
+          {text}
+        </p>
+      </div>
+    )
+  }
   return (
     <div>
       <div>
         <h2>Choose Font: </h2>
         <div>
-          <label htmlFor='Font-select'>Choose a Font</label><br />
-          <select id='Font-select' value={selectFont} onChange = { (e) => setSelectFont(e.target.value)}>
-            {fonts.map((font) => (
-              <option key={font} value={font}>
-                {font.split(',')[0]}
-              </option>
-            ))}
-          </select>
+          <label htmlFor='Font-select'>Choose a Font</label>
+          <div>
+            <select id='Font-select' value={selectFont} onChange = { (e) => setSelectFont(e.target.value)}>
+              {fonts.map((font) => (
+                <option key={font} value={font}>
+                  {font.split(',')[0]}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
         <h3>Personalization Below Photo:</h3>
         <textarea
           value = {text}
           onChange = { (e) => setText(e.target.value)}
         />
+        <button
+          onClick = {handleSubmit}
+        >
+          Submit
+        </button>
       </div>
     </div>
   )
-
 }
 
 export default App
